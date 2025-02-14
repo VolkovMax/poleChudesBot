14c5b4c13ab4cb3cf53e9c45f3e55937dd70a65c
Max <mvolkov@komus.net>
2025-02-14 12:41:05 +0300

    Сокрытие класса Constants

---
+++ src/main/java/games/fieldOfDreams/Constants.java
@@ -0,0 +1,11 @@
+package games.fieldOfDreams;
+
+import lombok.Getter;
+
+@Getter
+public class Constants
+{
+    public static final String BOT_NAME = "soKiss_bot";
+
+    public static final String BOT_TOKEN = "7862124068:AAH_4Pm0GRTS8h_-hXM6mW5ESNRxTc-zh9M";
+}
